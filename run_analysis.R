65ea0d5d30ed501fe4ce35747f5884b6ea753109
Alexander Minyushkin <alexander.minushkin@gmail.com>
2014-04-23 18:26:40 +0400

    Parsed labels and features for training set

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 
 setwd("~/GitHub/coursera-getdata-002")
+require(data.table)
 
 if( !any(dir() == "UCI HAR Dataset") ){
   cat("Unzipping getdata-projectfiles-UCI HAR Dataset.zip\n")
@@ -7,5 +8,27 @@ if( !any(dir() == "UCI HAR Dataset") ){
 }
 
 
+activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", header=FALSE, sep = "")
+names(activity_labels) <- c("id", "label")
 
-l <- readLines("UCI HAR Dataset/train/X_train.txt")
+features <- read.csv("UCI HAR Dataset/features.txt", header=FALSE, sep = "")
+names(features) <- c("id", "name")
+
+#Removing special characters from names
+# For example "tBodyAcc-mean()-X" become "tBodyAcc_mean_X"
+features$name <- gsub("\\W", "_", features$name)            
+features$name <- gsub("___", "_", features$name)  
+features$name <- gsub("__", "_", features$name)  
+features$name <- gsub("_$", "", features$name)  
+
+X_train <- read.csv("UCI HAR Dataset/train/X_train.txt", header=FALSE, sep = "")
+names(X_train) <- features$name
+  
+y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", header=FALSE, sep = "", col.names="label_id")
+subject_train<- read.csv("UCI HAR Dataset/train/subject_train.txt", header=FALSE, sep = "", col.names="id")
+
+train <- cbind( data.table(label = as.factor(activity_labels$label[y_train$label_id]),
+                          subject = subject_train$id,
+                          set="train"),
+                X_train)
+
