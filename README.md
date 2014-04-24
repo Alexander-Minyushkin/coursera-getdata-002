a5773eef13779b81abf1748a22711802375982f8
Alexander Minyushkin <alexander.minushkin@gmail.com>
2014-04-24 19:26:10 +0400

    README updated with references,  DRY implemented

---
+++ README.md
@@ -0,0 +1,17 @@
+## Peer assessment project for Coursera Getting and Cleaning Data course
+
+
+
+
+
+
+
+
+## References
+
+1. [Coursera Getting and Cleaning Data](https://class.coursera.org/getdata-002)
+2. [Task clarifications thread](https://class.coursera.org/getdata-002/forum/thread?thread_id=28)
+3. [Hint for results reporting](https://class.coursera.org/getdata-002/forum/thread?thread_id=416)
+4. [Markdown live demo used for *.md preview](http://markdown-here.com/livedemo.html)
+5. [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+6. [DRY principle](http://en.wikipedia.org/wiki/Don't_repeat_yourself)
--- run_analysis.R
+++ run_analysis.R
@@ -14,6 +14,9 @@ names(activity_labels) <- c("id", "label")
 features <- read.csv("UCI HAR Dataset/features.txt", header=FALSE, sep = "")
 names(features) <- c("id", "name")
 
+# IDs of the measurements on the mean and standard deviation for each measurement. 
+mean_and_std_IDs <- c( grep("mean", features$name, fixed=TRUE), grep("std", features$name, fixed=TRUE))
+
 #Removing special characters from names
 # For example "tBodyAcc-mean()-X" become "tBodyAcc_mean_X"
 features$name <- gsub("\\W", "_", features$name)            
@@ -21,14 +24,31 @@ features$name <- gsub("___", "_", features$name)
 features$name <- gsub("__", "_", features$name)  
 features$name <- gsub("_$", "", features$name)  
 
-X_train <- read.csv("UCI HAR Dataset/train/X_train.txt", header=FALSE, sep = "")
-names(X_train) <- features$name
+# Train an test folders have similar structure and file names. 
+# So to follow DRY principle (http://en.wikipedia.org/wiki/Don't_repeat_yourself) 
+# I am going to use one function to process both data sets.
+# As a helper I need to have function to get path to apropriate files.
+
+getPath <-function(f, set) { paste0("UCI HAR Dataset/", set,"/", f, "_", set,".txt")}
+
+processSet <- function(set){
+  X <- read.csv(getPath("X", set), header=FALSE, sep = "")
+  names(X) <- features$name
   
-y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", header=FALSE, sep = "", col.names="label_id")
-subject_train<- read.csv("UCI HAR Dataset/train/subject_train.txt", header=FALSE, sep = "", col.names="id")
+  #2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+  mean_and_std <- X[mean_and_std_IDs]
+    
+  y <- read.csv(getPath("y", set), header=FALSE, sep = "", col.names="label_id")
+  subject<- read.csv(getPath("subject", set), header=FALSE, sep = "", col.names="id")
+  
+  out <- cbind( data.table(label = as.factor(activity_labels$label[y$label_id]),
+                            subject = subject$id,
+                            set),
+                  mean_and_std)
+}
 
-train <- cbind( data.table(label = as.factor(activity_labels$label[y_train$label_id]),
-                          subject = subject_train$id,
-                          set="train"),
-                X_train)
+train <- processSet("train")
+test <- processSet("test")
 
+result = rbind(train, test)
+result$set <- as.factor(result$set)
