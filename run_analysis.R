baf023b4e10dcdf9619c42775539526076d9a06c
Alexander Minyushkin <alexander.minushkin@gmail.com>
2014-04-25 15:21:43 +0400

    tidy data set for project submission
    Implemented generation of independent tidy data set with the average of
    each variable for each activity and each subject.

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,18 @@
 
 setwd("~/GitHub/coursera-getdata-002")
 require(data.table)
+require(plyr)
 
 if( !any(dir() == "UCI HAR Dataset") ){
   cat("Unzipping getdata-projectfiles-UCI HAR Dataset.zip\n")
   unzip("getdata-projectfiles-UCI HAR Dataset.zip")
 }
 
-
+cat("Reading activity_labels.txt\n")
 activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", header=FALSE, sep = "")
-names(activity_labels) <- c("id", "label")
+names(activity_labels) <- c("id", "activity")
 
+cat("Reading features.txt\n")
 features <- read.csv("UCI HAR Dataset/features.txt", header=FALSE, sep = "")
 names(features) <- c("id", "name")
 
@@ -41,14 +43,32 @@ processSet <- function(set){
   y <- read.csv(getPath("y", set), header=FALSE, sep = "", col.names="label_id")
   subject<- read.csv(getPath("subject", set), header=FALSE, sep = "", col.names="id")
   
-  out <- cbind( data.table(label = as.factor(activity_labels$label[y$label_id]),
+  out <- cbind( data.table(activity = as.factor(activity_labels$activity[y$label_id]),
                             subject = subject$id,
                             set),
                   mean_and_std)
 }
 
+cat("Reading train set\n")
 train <- processSet("train")
+
+cat("Reading test set\n")
 test <- processSet("test")
 
 result = rbind(train, test)
 result$set <- as.factor(result$set)
+
+independent_dataset <- ddply(result, .(activity, subject), .fun = function(x){ colMeans(x[, 4:82], na.rm = TRUE)})
+
+
+# Unit test:
+# Alternative way to compute mean value is used to verify one value from independent_dataset
+ if( independent_dataset[1,]$tBodyAcc_mean_X == 
+       mean(result[activity=="LAYING" & subject==1,]$tBodyAcc_mean_X)){
+   cat("Unit test passed\n")
+ }else{
+    cat("Unit test FAILED!!!!!\n")
+  }
+
+
+write.table(independent_dataset, file="./independent_tidy_data_set.txt", sep="\t", row.names=FALSE)
